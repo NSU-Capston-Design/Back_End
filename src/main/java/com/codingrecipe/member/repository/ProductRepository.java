24fc55e225743bc73af0da326268280d54115d4e
PBEM22 <dlacofbs1229@gmail.com>
2024-04-24 23:59:05 +0900

    [Fix] 주문 Dto 생성, [Feat] 주문 취소, 주문 상세 조회

--- src/main/java/com/codingrecipe/member/repository/ProductRepository.java
+++ src/main/java/com/codingrecipe/member/repository/ProductRepository.java
@@ -7,18 +7,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     @Query(value = "SELECT p FROM ProductEntity p ORDER BY p.productView DESC")
-    List<ProductEntity> findTop3ByProductView();
+    Optional<List<ProductEntity>> findTop3ByProductView();
 
     @Query(value = "SELECT p FROM ProductEntity p where p.fileId = :fileId")
-    ProductEntity findByFileId(Long fileId);
+    Optional<ProductEntity> findByFileId(Long fileId);
 
     @Query(value = "select p from ProductEntity p join fetch p.memberEntity")
-    List<ProductEntity> findByMemberEntityOfProductEntity();
+    Optional<List<ProductEntity>> findByMemberEntityOfProductEntity();
 
     @Query(value = "select p from ProductEntity p where p.fileId IN :productId")
     List<ProductEntity> findAllByProductId(long[] productId);
