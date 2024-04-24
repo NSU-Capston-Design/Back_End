cfa4e46cc2b8a51b8d1cc509e9c0753d8675d48f
PBEM22 <dlacofbs1229@gmail.com>
2024-04-24 23:59:05 +0900

    [Fix] 주문 Dto 생성, [Feat] 주문 취소, 주문 상세 조회

--- src/main/java/com/codingrecipe/member/controller/OrderController.java
+++ src/main/java/com/codingrecipe/member/controller/OrderController.java
@@ -1,10 +1,12 @@
 package com.codingrecipe.member.controller;
 
 import com.codingrecipe.member.dto.order.OrderDTO;
+import com.codingrecipe.member.dto.order.OrderItmesDTO;
 import com.codingrecipe.member.dto.order.OrderRequestDTO;
 import com.codingrecipe.member.entity.OrderEntity;
 import com.codingrecipe.member.exception.NotEnoughInvenException;
 import com.codingrecipe.member.exception.NotFoundMemberException;
+import com.codingrecipe.member.exception.NotFoundOrderException;
 import com.codingrecipe.member.exception.NotFoundProductException;
 import com.codingrecipe.member.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +30,9 @@ public class OrderController {
      * 주문하기
      */
     @PostMapping(value = "/order")
-    public ResponseEntity<String> createOrder(@RequestBody OrderRequestDTO orderDTO){
+    public ResponseEntity<String> createOrder(@RequestBody OrderItmesDTO orderItmesDTO){
         try {
-            orderService.createOrder(orderDTO.getPrice(), orderDTO.getCount(), orderDTO.getMemberId(), orderDTO.getProductId());
+            orderService.createOrder(orderItmesDTO);
         } catch (NotEnoughInvenException | NotFoundProductException | NotFoundMemberException e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -41,7 +43,7 @@ public class OrderController {
      * 주문 내역 조회
      */
     @GetMapping("/orders")
-    public ResponseEntity<?> getOrderById(@RequestParam("userId") String userId){
+    public ResponseEntity<?> getAllOrders(@RequestParam("userId") String userId){
         try {
             List<OrderDTO> orders = orderService.orders(userId);
             return ResponseEntity.ok(orders);
@@ -50,18 +52,24 @@ public class OrderController {
         }
     }
 
-    @GetMapping("/getAll")
-    public List<OrderRequestDTO> getAllOrders() {
-        return orderService.getAllOrders();
+    /**
+     * 주문 상세 조회
+     */
+    @GetMapping("/orders/{orderId}")
+    public ResponseEntity<?> getOrder(@PathVariable("orderId") long orderId) {
+        try {
+            OrderDTO order = orderService.getOrder(orderId);
+            return ResponseEntity.ok(order);
+        } catch (NotFoundOrderException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
-    @PutMapping("/update/{orderId}")
-    public OrderEntity updateOrder(@PathVariable Long orderId, @RequestBody OrderRequestDTO orderDTO) {
-        return orderService.updateOrder(orderId, orderDTO);
-    }
-
-    @DeleteMapping("/delete/{orderId}")
-    public void deleteOrder(@PathVariable Long orderId) {
+    /**
+     * 주문 취소
+     */
+    @DeleteMapping("/order/delete/{orderId}")
+    public void deleteOrder(@PathVariable("orderId") long orderId) {
         orderService.deleteOrder(orderId);
     }
 }
--- src/main/java/com/codingrecipe/member/dto/order/OrderRequestDTO.java
+++ src/main/java/com/codingrecipe/member/dto/order/OrderRequestDTO.java
@@ -4,9 +4,8 @@ import lombok.Data;
 
 @Data
 public class OrderRequestDTO {
-    private long memberId;
-    private int[] count;
-    private int[] price;
-    private long[] productId;
+    private int count;
+    private int price;
+    private long productId;
 
 }
