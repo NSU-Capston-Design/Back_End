f86ed9a145f03b0f2a9efe271c5ef489417bb639
PBEM22 <dlacofbs1229@gmail.com>
2024-04-26 17:52:11 +0900

    [Fix] Donation

--- src/main/java/com/codingrecipe/member/repository/DonationRepository.java
+++ src/main/java/com/codingrecipe/member/repository/DonationRepository.java
@@ -1,15 +1,19 @@
 package com.codingrecipe.member.repository;
 
 import com.codingrecipe.member.entity.DonationEntity;
+import com.codingrecipe.member.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface DonationRepository extends JpaRepository<DonationEntity, Long> {
-    List<DonationEntity> findAllByUserId(String userId);
+    @Query("SELECT d FROM DonationEntity d where d.member = :member")
+    List<DonationEntity> findAllByUserId(MemberEntity member);
     List<DonationEntity> findAllByDonationDate(LocalDate donationDate);
     List<DonationEntity> findAllByAmount(int amount);
 
-
+    @Query("SELECT d FROM DonationEntity d WHERE d.member = :member")
+    List<DonationEntity> findAllByMember(MemberEntity member);
 }
