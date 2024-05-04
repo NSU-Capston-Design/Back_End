4b6d01fe41385b91fcb4c94c08f2c8b8fb6daae9
PBEM22 <dlacofbs1229@gmail.com>
2024-05-04 23:41:50 +0900

    [Fix] orderService, DTO 수정 및 완료

--- src/main/java/com/codingrecipe/member/repository/OrderItemRepository.java
+++ src/main/java/com/codingrecipe/member/repository/OrderItemRepository.java
@@ -2,7 +2,14 @@ package com.codingrecipe.member.repository;
 
 import com.codingrecipe.member.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    @Query("SELECT o FROM OrderItem o where o.order.orderId = :orderId")
+    List<OrderItem> findOrderItemByOrderId(long orderId);
 
 }
--- src/main/java/com/codingrecipe/member/service/OrderService.java
+++ src/main/java/com/codingrecipe/member/service/OrderService.java
@@ -16,13 +16,19 @@ import com.codingrecipe.member.repository.MemberRepository;
 import com.codingrecipe.member.repository.OrderItemRepository;
 import com.codingrecipe.member.repository.OrderRepository;
 import com.codingrecipe.member.repository.ProductRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -57,7 +63,7 @@ public class OrderService {
             Optional<ProductEntity> byFileId = productRepository.findByFileId(orderRequestDTO.getProductId());
             if (byFileId.isEmpty()){        // byfileId가 존재 안할시에 예외처리
                 log.info("===상품이 존재하지 않음=== fileId : " + orderRequestDTO.getProductId());
-                throw new NotFoundProductException("상품을 찾을 수 없습니다.", byFileId.get().getFileId());
+                throw new NotFoundProductException("상품을 찾을 수 없습니다.", orderRequestDTO.getProductId());
             }
             products.add(byFileId.get());           // List product에 추가
         }
@@ -73,18 +79,26 @@ public class OrderService {
         List<OrderItem> orderItems = new ArrayList<>(orderDTOS.size());                             // orderItem을 생성 후 담을 List 생성
         log.info("===List<orderItems>에 orderItem 생성 후 넣기===");
 
-        if (orderDTOS.size() == 1){
-            log.info("===orderDTOS.size가 1일때===");
-            OrderItem orderItem = OrderItem.createOrderItem(products.get(0), orderDTOS.get(0).getPrice(), orderDTOS.get(0).getCount());
-            orderItems.add(orderItem);
-
-            log.info("===OrderEntity 생성===");
-            OrderEntity order = OrderEntity.createOrder(memberEntity, orderItems);
-
-            log.info("===OrderEntity 저장===");
-            orderRepository.save(order);
-        }
-        else {
+//        if (orderDTOS.size() == 1){
+//            log.info("===orderDTOS.size가 1일때===");
+//            OrderItem orderItem = OrderItem.createOrderItem(products.get(0), orderDTOS.get(0).getPrice(), orderDTOS.get(0).getCount());
+//            orderItems.add(orderItem);
+//
+//            log.info("===OrderEntity 생성===");
+//            OrderEntity order = OrderEntity.createOrder(memberEntity, orderItems);
+//
+//            log.info("===OrderEntity 저장===");
+//            orderRepository.save(order);
+//
+//            log.info("===OrderItem에 OrderEntity set===");
+//            for (OrderItem orderItem : orderItems){
+//                orderItem.setOrder(order);
+//            }
+//
+//            log.info("===orderItem 저장===");
+//            orderItemRepository.saveAll(orderItems);
+//        }
+//        else {
             for (int i = 0; i < orderDTOS.size(); i++){
                 log.info("===orderItem 생성=== i : " + i);
                 OrderItem orderItem = OrderItem.createOrderItem(products.get(i), orderDTOS.get(i).getPrice(), orderDTOS.get(i).getCount());
@@ -100,11 +114,12 @@ public class OrderService {
             log.info("===OrderItem에 OrderEntity set===");
             for (OrderItem orderItem : orderItems){
                 orderItem.setOrder(order);
+                System.out.println("117line => orderItem.getCount : " + orderItem.getCount());
             }
 
             log.info("===orderItem 저장===");
             orderItemRepository.saveAll(orderItems);
-        }
+//        }
     }
 
     /**
@@ -127,32 +142,38 @@ public class OrderService {
 
         List<OrderDTO> orderDTOS = new ArrayList<>();
 
-        for (int i = 0; i < orders.size(); i++){
-            log.info("===orderDTOList에 orderEntity 집어넣기=== i : " + i);
-            OrderEntity orderEntity = orders.get(i);
+        for (OrderEntity order : orders) {
+            log.info("===orderDTOList에 orderEntity 집어넣기=== ordersId : " + order.getOrderId());
 
+            String date = localDateToDate(order.getOrderDate());// 변환한 날짜 저장
+
+            // i번째 orderId로 orderItems 조회
+            List<OrderItem> orderItems = orderItemRepository.findOrderItemByOrderId(order.getOrderId());
             List<OrderItemsResDTO> orderDTOList = new ArrayList<>();
 
-            for (int j = 0; j < orderEntity.getOrderItems().size(); j++){
+            for (OrderItem orderItem : orderItems) {
+
+                log.info("===orderItem Id=== : " + orderItem.getId());
+
                 OrderItemsResDTO build = OrderItemsResDTO.builder()
-                        .id(orderEntity.getOrderItems().get(i).getId())
-                        .orderPrice(orderEntity.getOrderItems().get(i).getOrderPrice())
-                        .count(orderEntity.getOrderItems().get(i).getCount())
-                        .productName(orderEntity.getOrderItems().get(i).getProduct().getProductName())
-                        .productId(orderEntity.getOrderItems().get(i).getProduct().getFileId())
+                        .id(orderItem.getId())
+                        .orderPrice(orderItem.getOrderPrice())
+                        .count(orderItem.getCount())
+                        .productName(orderItem.getProduct().getProductName())
+                        .productId(orderItem.getProduct().getFileId())
                         .build();
 
+                log.info("===productName=== : " + build.getProductName());
                 orderDTOList.add(build);
             }
 
-
             OrderDTO orderDTO = OrderDTO.builder()
-                    .orderId(orderEntity.getOrderId())
-                    .orderDate(orderEntity.getOrderDate())
-                    .orderStatus(orderEntity.getOrderStatus())
-                    .delivery(orderEntity.getDelivery())
+                    .orderId(order.getOrderId())
+                    .orderDate(date)
+                    .orderStatus(order.getOrderStatus())
+                    .delivery(order.getDelivery())
                     .orderItems(orderDTOList)
-                    .orderTotalCost(orderEntity.getTotalPrice())
+                    .orderTotalCost(order.getTotalPrice())
                     .build();
 
             log.info("===orderDTOList에 넣기 성공===");
@@ -176,6 +197,9 @@ public class OrderService {
         OrderEntity orderEntity = byId.get();
         log.info("===[Builder]OrderEntity를 OrderDTO로 변환===");
 
+
+        String date = localDateToDate(orderEntity.getOrderDate()); // 날짜 포맷 변환 및 String으로 변환
+
         List<OrderItemsResDTO> orderItemsResDTOS = new ArrayList<>();
 
         log.info("===orderItems.size=== size : " + orderEntity.getOrderItems().size());
@@ -195,7 +219,7 @@ public class OrderService {
         log.info("===OrderDTO 반환===");
         return   OrderDTO.builder()
                 .orderId(orderEntity.getOrderId())
-                .orderDate(orderEntity.getOrderDate())
+                .orderDate(date)
                 .orderStatus(orderEntity.getOrderStatus())
                 .delivery(orderEntity.getDelivery())
                 .orderItems(orderItemsResDTOS)
@@ -236,6 +260,19 @@ public class OrderService {
         for (int i = 0; i < byId.get().getOrderItems().size(); i++){
             byId.get().getOrderItems().get(i).setCount(count);
         }
+    }
 
+    /**
+     * LocalDate => Instant => Date => String
+     * @return String
+     */
+    private String localDateToDate(LocalDateTime local){
+
+        SimpleDateFormat format = new SimpleDateFormat("yy/MM/dd - HH:mm"); // 형식 변환
+
+        Instant instant = local.atZone(ZoneId.systemDefault()).toInstant();
+        Date from = Date.from(instant);
+
+        return format.format(from);
     }
 }
